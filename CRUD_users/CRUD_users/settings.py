65a7bc5e37e465fe65f41588ae447f2e3219bef9
Christian Mendez <christian.mndz@outlook.com>
2023-01-17 18:37:26 -0600

    .

--- CRUD_users/CRUD_users/settings.py
+++ CRUD_users/CRUD_users/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'CRUD_users/CRUD_users/api.apps.ApiConfig',
+    'CRUD_users.CRUD_users.api.apps.ApiConfig',
     'rest_framework'
 ]
 
