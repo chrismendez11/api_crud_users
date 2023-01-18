ef6bc7c75ab9e80c6ab7d7702c16aa46d7429358
Christian Mendez <christian.mndz@outlook.com>
2023-01-17 18:49:14 -0600

    done

--- CRUD_users/CRUD_users/settings.py
+++ CRUD_users/CRUD_users/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from ..api import apps
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    '../api.apps.ApiConfig',
+    f'{apps}.ApiConfig',
     'rest_framework'
 ]
 
