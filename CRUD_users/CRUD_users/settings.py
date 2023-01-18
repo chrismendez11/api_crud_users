aee2eed2d7bbcb918812f08caf57b83b460c47c0
Christian Mendez <christian.mndz@outlook.com>
2023-01-18 07:12:40 -0600

    .

--- CRUD_users/CRUD_users/settings.py
+++ CRUD_users/CRUD_users/settings.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import os
 import dj_database_url
-from api import apps
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+ApiConfig = os.path.join(BASE_DIR, 'ApiConfig')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -31,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    f'{apps.ApiConfig}',
+    f'{ApiConfig}',
     'rest_framework',
 ]
 
